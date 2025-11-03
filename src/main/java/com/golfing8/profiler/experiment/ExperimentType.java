ce32d4e19695491b30845a698069c79a6693b64e
Golfing7 <atroo@me.com>
2025-11-03 16:02:06 -0500

    Add NULL experiment type

--- src/main/java/com/golfing8/profiler/experiment/ExperimentType.java
+++ src/main/java/com/golfing8/profiler/experiment/ExperimentType.java
@@ -8,6 +8,7 @@ import java.util.function.Function;
 
 @AllArgsConstructor @Getter
 public enum ExperimentType {
+    NULL((world) -> new Experiment("null", world, null)),
     FLOOD((world) -> new Experiment("flood", world, "flood.schem")),
     FLOOD_3D((world) -> new Experiment("flood_3d", world, "flood_3d.schem")),
     LINE((world) -> new Experiment("line", world, "line.schem")),
